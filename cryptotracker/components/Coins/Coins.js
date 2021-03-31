1b0dbb114c52969b88c6e6053519c1e8453f8f70
Damjan15 <stamenovdamjan20@gmail.com>
2021-03-31 13:12:59 +0200

    Layout added

--- cryptotracker/components/Coins/Coins.js
+++ cryptotracker/components/Coins/Coins.js
@@ -1,9 +1,41 @@
-const Coins = ({ name }) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-        </div>
-    )
-}
+import styles from "./Coins.module.css";
 
-export default Coins
+const Coins = ({
+  id,
+  name,
+  price,
+  volume,
+  image,
+  symbol,
+  priceChange,
+  marketcap,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.coin}>
+          <img src={image} alt={name} className={styles.img} />
+          <h1 className={styles.h1}>{name}</h1>
+          <p className={styles.symbol}>{symbol}</p>
+        </div>
+
+        <div className={styles.data}>
+          <p className={styles.price}>${price}</p>
+          <p className={styles.volume}>${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className={styles.percent_red}>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className={styles.percent_green}>{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className={styles.marketcap}>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coins;
--- cryptotracker/pages/index.js
+++ cryptotracker/pages/index.js
@@ -1,31 +1,28 @@
-import Head from 'next/head'
-import CoinList from '../components/CoinList'
-import Coins from '../components/Coins/Coins'
-import SearchBar from '../components/SearchBar/SearchBar'
+import CoinList from "../components/CoinList";
+import Layout from "../components/Layout";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 export default function Home({ filteredCoins }) {
-  { console.log(filteredCoins) }
   return (
-    <div>
-      <Head>
-        <title>Home | Crypto Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <SearchBar type="text" placeholder="Search" />
-      <CoinList filteredCoins={filteredCoins} />
-    </div>
-  )
+    <Layout>
+      <div className="coin__app">
+        <SearchBar type="text" placeholder="Search" />
+        <CoinList filteredCoins={filteredCoins} />
+      </div>
+    </Layout>
+  );
 }
 
-export const getServerSideProps = async() => {
-  const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  );
 
   const filteredCoins = await res.json();
 
   return {
-      props: {
-          filteredCoins
-      }
-  }
-}
+    props: {
+      filteredCoins,
+    },
+  };
+};
