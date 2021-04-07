78158377329b44814bd38222d0bd12a6b72b5392
Damjan15 <stamenovdamjan20@gmail.com>
2021-04-07 16:55:42 +0200

    Homepage added

--- mangaworld/src/Components/HomeNav/HomeNav.js
+++ mangaworld/src/Components/HomeNav/HomeNav.js
@@ -1,26 +1,27 @@
 import { Link } from "react-router-dom";
 import { BsBook } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 import "./homenav.css";
 
 const HomeNav = () => {
-    return (
-        <div className="homeNav">
-            <div className="homeNav__left">
-                <BsBook />
-            </div>
-            <ul className="homeNav__right">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/">About</Link>
-                </li>
-                <li>
-                    <Link to="/">Explore Books</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="homeNav">
+      <div className="homeNav__left">
+        <BsBook />
+      </div>
+      <div className="mobileIcon">
+        <AiOutlineMenu />
+      </div>
+      <ul className="homeNav__right">
+        <li>
+          <Link to="/">Explore Books</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default HomeNav
+export default HomeNav;
--- mangaworld/src/Pages/Home/Home.js
+++ mangaworld/src/Pages/Home/Home.js
@@ -1,11 +1,29 @@
-import Hero from "../../Components/Hero/Hero"
-import HomeNav from "../../Components/HomeNav/HomeNav"
-import HomeToggle from "../../Components/HomeToggle/HomeToggle"
+import HomeNav from "../../Components/HomeNav/HomeNav";
+import { Link } from "react-router-dom";
+import HeroImage from "../../assets/HeroImage.png";
+import "./home.css";
 
 export const Home = () => {
-    return (
-        <div>
-            <Hero />
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="hero__wrapper">
+          <div className="hero__left">
+            <HomeNav />
+            <h1>Read Your Favorite <span>Manga</span></h1>
+            <p>
+              All of your favorite mangas in one place. From action to comedy we
+              have it all here
+            </p>
+            <Link to="/" className="hero__btn">
+              Discover Books
+            </Link>
+          </div>
+          <div className="hero__right">
+            <img src={HeroImage} alt="" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
